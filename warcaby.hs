cdcf0bfe78bdaf26c330f3831864d63e05a4542a
M!nified <gora.pub@gmail.com>
2016-05-11 13:57:57 +0200

    getElems

--- warcaby.hs
+++ warcaby.hs
@@ -2,11 +2,11 @@ import Data.List.Split
 import Data.List
 
 main = do
-    let dane = parseGame initB
-    pokaz dane
-    print "Hello"
+    pokazPlansze pl
+    print pl
     
 initB = ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
+pl = Plansza (parseGame initB) 12 12
 
 
 everyNth lst n = [snd x | x <- zip [1 ..] lst, fst x `mod` n == 0]
@@ -14,6 +14,8 @@ everyNth lst n = [snd x | x <- zip [1 ..] lst, fst x `mod` n == 0]
 parseElem::String -> Pole
 parseElem "b" = Pole 0 0 Pionek Czarne
 parseElem "w" = Pole 0 0 Pionek Biale
+parseElem "B" = Pole 0 0 Damka Biale
+parseElem "W" = Pole 0 0 Damka Czarne
 parseElem _ = Pole 0 0 Wolne Brak
 
 blowLine line = filter (/= "") (splitOn ""  line)--(\x -> x /= "")
@@ -31,38 +33,61 @@ findPositions [[]] _ = [[]]
 findPositions [] _ = [[]]
 findPositions mapa y = do
     let t = tail mapa
-    let tp = runFindPositions (t)
+    let tp = runFindPositions t
     let h = runFindPositionsInRow (head mapa) y
-    let tpp = if length tp > 0
+    let tpp = if not (null tp)
         then tp
         else []
     return h++tpp
     
-runFindPositionsInRow row y = findPositionsInRow row (length row) y
+runFindPositionsInRow row = findPositionsInRow row (length row)
 findPositionsInRow :: [Pole] -> Int -> Int -> [Pole]
 findPositionsInRow [] _ _ = []
 findPositionsInRow row x y = do
     let t = tail row
     let h = head row
-    let hp = Pole x y (typ h) (kolor h)
+    let hp = Pole (8-x) (8-y) (typ h) (kolor h)
     let tp = runFindPositionsInRow t y
     return hp++tp
-    
-    
---pokaz plansze
-pokaz = mapM print
 
 data Kolor = Biale | Czarne | Brak
     deriving (Show)
-data TypNaDanymPolu = Pionek | Krolowa | Wolne
+data TypNaDanymPolu = Pionek | Damka | Wolne
     deriving (Show)
 data Pole = Pole { x :: Int, y :: Int, typ :: TypNaDanymPolu, kolor :: Kolor }
-  -- deriving (Show)
+--   deriving (Show)
 instance Show Pole where
     show Pole{kolor = Biale, typ = Pionek} = show 'w'
-    show Pole{kolor = Biale, typ = Krolowa} = show "W"
+    show Pole{kolor = Biale, typ = Damka} = show "W"
     show Pole{kolor = Czarne, typ = Pionek} = show "b"
-    show Pole{kolor = Czarne, typ = Krolowa} = show "B"
+    show Pole{kolor = Czarne, typ = Damka} = show "B"
     show Pole{} = show " "
     
+--plansza to [[Pole]]
+data Plansza = Plansza {
+    plansza :: [[Pole]],
+    numB :: Int,
+    numW :: Int
+    } deriving (Show)
+--pokaz plansze
+pokazPlansze pl= mapM print (plansza pl)
+
 --poruszanie 
+
+--szukanie mozliwych ruchow
+--moves f board = (genKill f b, genMoves f b)
+-- genMoves::Pole -> Plansza -> Int
+getElemAt pl x y = pl !! (y-1) !! (x-1) 
+getElemAt2 pl = getElemAt (plansza pl)
+--up left
+getElemUL Plansza{plansza=pl} x y = getElemAt pl (x-1) (y-1)
+--up right
+getElemUR Plansza{plansza=pl} x y = getElemAt pl (x+1) (y-1)
+--down left
+getElemDL Plansza{plansza=pl} x y = getElemAt pl (x-1) (y+1)
+--down right
+getElemDR Plansza{plansza=pl} x y = getElemAt pl (x+1) (y+1)
+
+genMoves Pole{x=x,y=y,typ=Pionek,kolor=kolor} pl =
+    let r = [] ++ getElemUL pl x y ++ getElemUR pl x y
+    filter () r
